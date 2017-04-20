89045cd031ca1d5b6d6d251b77e9169c3e261a89
Takuya Saito <t-saito@mesena-net.com>
2017-04-21 01:43:44 +0900

    投稿画面を表示させる

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,7 @@
 class ReviewsController < RankingController
   def new
+    # binding.pry
+    @product = Product.find(params[:product_id])
+    @review = Review.new()
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ TechReviewSite::Application.routes.draw do
   root 'products#index'
   get 'products/search' => 'products#search'
   resources :products, only: [:show] do
-    resources :reviews, only: [:new]
+    resources :reviews, only: [:new, :create]
   end
 
 end
