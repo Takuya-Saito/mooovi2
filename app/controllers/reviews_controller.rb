cca5bcc625270b3515761bf7b36a82096b9aa288
Takuya Saito <t-saito@mesena-net.com>
2017-04-21 02:21:18 +0900

    投稿をreviewsテーブルに保存

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -4,4 +4,17 @@ class ReviewsController < RankingController
     @product = Product.find(params[:product_id])
     @review = Review.new()
   end
+
+  def create
+    binding.pry
+    review = Review.new(review_params)
+    review.save
+  end
+
+  private
+  def review_params
+    params.require(:review).permit(:nickname, :rate, :review).merge(product_id: params[:product_id])
+  end
+
+
 end
